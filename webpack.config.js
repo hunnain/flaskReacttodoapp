1211238fb8295ca5c0a98a725a512eb52fd0cce3
hunnainpasha <hunnainpashahgchgc@gmail.com>
2018-08-17 20:57:16 +0500

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const config = {
+    entry:  __dirname + '/js/index.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+          },
+
+            {
+                test: /\.css?$/,
+                loaders: [ 'style-loader', 'css-loader' ],
+            },
+            {
+                test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader : 'file-loader'
+            }
+          ]
+          
+      },
+};
+module.exports = config;
